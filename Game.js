5e893b6656b9dbae7562991841673ab67f8882bd
oneBoz <yiheding@gmail.com>
2021-07-17 14:31:46 +0800

    updated dino with long jump

--- Game.js
+++ Game.js
@@ -2,9 +2,12 @@ let groundHeight = 100;
 let dino;
 let cactus;
 let obstacles = [];
+let spaceDownTimer = 0;
+let spaceDown = false;
 
 function setup() {
   createCanvas(600, 400);
+  frameRate(60);
   dino = new Dino();
   for (let i = 0; i < 2; i++) {
     let c = new Cactus();
@@ -24,12 +27,19 @@ function draw() {
     let c = new Cactus();
     obstacles.push(c);
   }
+  
+  if (spaceDown) {
+    spaceDownTimer++;
+  }
+  
+
+  
 }
 
 //displays everything on screen
 function drawToScreen() {
   background(0);
-  stroke(255);
+  stroke(255);   
   strokeWeight(3);
   line(0, height - groundHeight, width, height - groundHeight);
 }
@@ -38,8 +48,31 @@ function keyPressed() {
   switch (key) {
     case " ":
       if (dino.y == 0) {
-        dino.velY = 10;
+        dino.velY = 16; 
+        dino.gravity = 0.8;
+        spaceDown = true;
+        spaceDownTimer = 0;
       }
       break;
   }
 }
+
+function keyReleased() {
+  switch (key) {
+    case " ":
+      if (spaceDownTimer < 13) {
+        dino.gravity = 1.5;
+      }
+      spaceDown = false;
+      break;
+  }
+}
+
+
+
+
+
+
+
+
+

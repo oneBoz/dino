0a07753527d88156ffc4c2d79d68a003d1b29472
rocketninja7 <andrewyapp@gmail.com>
2021-08-18 21:27:56 +0800

    Added Bird and DoubleCactus obstacles, minor bug fixes

--- obstacles.js
+++ obstacles.js
@@ -11,7 +11,49 @@ class Cactus {
   show(){
     fill(255);
     noStroke();
-    rect(this.x, height - groundHeight - this.h, this.w, this.h);
+    rect(this.x, height - groundHeight - this.h - this.y, this.w, this.h);
+  }
+  
+  move(){
+    this.x += this.xSpeed;
+  }
+}
+
+class DoubleCactus {
+  constructor(){
+    this.x = width;
+    this.y = 0;
+    this.h = 50;
+    this.w = 50
+    
+    this.xSpeed = -5;
+  }
+  
+  show(){
+    fill(255);
+    noStroke();
+    rect(this.x, height - groundHeight - this.h - this.y, this.w, this.h);
+  }
+  
+  move(){
+    this.x += this.xSpeed;
+  }
+}
+
+class Bird {
+  constructor(){
+    this.x = width;
+    this.y = 40 * Math.ceil(Math.random() * 2);
+    this.h = 25;
+    this.w = 25
+    
+    this.xSpeed = -5;
+  }
+  
+  show(){
+    fill(255);
+    noStroke();
+    rect(this.x, height - groundHeight - this.h - this.y, this.w, this.h);
   }
   
   move(){
